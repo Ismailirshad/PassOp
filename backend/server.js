ace591188da65111c5497370f72bba74a1a3fa0c
Ismailirshad <irshadsha164@gmail.com>
2025-11-07 12:05:21 +0530

    moved backend dependencies to root

--- backend/server.js
+++ backend/server.js
@@ -80,9 +80,10 @@ async function startServer() {
       }
     });
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist/index.html"));
-    });
+   app.get("(.*)", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
 
     app.listen(port, () => {
       console.log(` Server is running on port ${port}`);
